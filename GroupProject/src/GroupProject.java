b06a9468ac74b9d1879a462953202b6c707a5562
16076006 <16076006@L815WD220013.edu.cbe.ab.ca>
2022-05-19 14:46:21 -0600

    2:46PM

--- GroupProject/src/GroupProject.java
+++ GroupProject/src/GroupProject.java
@@ -1,9 +1,24 @@
+/*
+Program: GroupProject       Date:18/05/2022
+Author: Seth Mckenzie 
+School: CHHS
+Course: Computer Science 10
+ 
+*/
 
 public class GroupProject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+		
+		
 	}
 
 }
+
+/* Screen Dump
+
+
+
+*/
